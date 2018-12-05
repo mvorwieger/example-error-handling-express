9c42d3355fd3a548e4298288c5e87d32229458f8
mvorwieger <michel.vorwieger@outlook.de>
2018-12-05 18:51:38 +0100

    added Winston logger

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express")
+const winston = require("winston")
 const app = express()
+// This module wraps all our routes in a A Error wrapper and delegates it to our ErrorMiddleware 
+require("express-async-errors")
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.File({ filename: "logfile.log" })
+    ]
+})
 
 app.use(express.json())
 
-app.route("/testRoute/:id", (res, req) => {
-
+app.get("/testRoute", (res, req) => {
+    throw new Error("test error 123")
 })
 
+// ErrorMiddleware
 // You should move this error logging middleware in a seperate File
 app.use((err, req, res, next) => {
+    logger.error(err.message, err)
     res.status(500).send("Internal Server Error")
 })
+
+app.listen(3000, () => console.log("started at localhost:3000"))
